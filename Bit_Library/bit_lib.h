99263767855b682091a041ef2d2cf6d32f1af6d7
ExpIMD <fourexpimd@gmail.com>
2025-07-11 23:06:43 +0300

    Implementation of bit printing methods and bug fixes

--- Bit_Library/bit_lib.h
+++ Bit_Library/bit_lib.h
@@ -5,16 +5,34 @@
 #include <cstddef>
 #include <string>
 
+using namespace std::string_literals;
+
 namespace IMD {
 	template<typename T>
 	void print_bytes(const T& value, const std::string& separator = " "s) {
-		auto ptr = reinterpret_cast<std::byte*>(&value);
+		auto ptr = reinterpret_cast<const std::byte*>(&value);
 		for (size_t i{ 0 }; i < sizeof(T); ++i)
-			std::cout << static_cast<short>(*(ptr + i)) << separator;
+			std::cout << static_cast<short>(ptr[i]) << separator;
 	}
 	template<typename T>
 	void println_bytes(const T& value, const std::string& separator = " "s) {
 		print_bytes(value, separator);
+		std::cout << std::endl;
+	}
+
+	template<typename T>
+	void print_bits(const T& value, const std::string& separator = " "s) {
+		auto ptr = reinterpret_cast<const std::byte*>(&value);
+		for (size_t i{ 0 }; i < sizeof(T); ++i) {
+			for (short j{ 7 }; j >= 0; --j)
+				std::cout << (((static_cast<short>(ptr[i]) >> j) & 1));
+			std::cout << separator;
+		}
+	}
+
+	template<typename T>
+	void println_bits(const T& value, const std::string& separator = " "s) {
+		print_bits(value, separator);
 		std::cout << std::endl;
 	}
 
---
+++ Bit_Library/program.cpp
@@ -0,0 +1,6 @@
+#include "bit_lib.h"
+
+int main() {
+	int value{ 256 };
+	IMD::println_bits(value);
+}
